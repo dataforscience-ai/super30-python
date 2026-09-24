235779c61295f1b4d281de7eb07ec7d638bbae85
dataforscience-ai <dataforscience@gmail.com>
2026-09-24 22:42:45 +0530

    Add files via upload

---
+++ list_tuple_queue.py
@@ -0,0 +1,24 @@
+# LISTS
+skills = ["Analytics", "People Management", "AI", "Pr"]
+favourite_foods = ["Dosa", "Idli", "Pongal", "Upma"]
+learning_topics = ["Python", "Generative AI", "GitHub", "Agentic AI"]
+# TUPLES
+location = ("Bangalore", "Karnataka", "India")
+education = ("B.Tech Biotechnology", "Calicut University", "India")
+office_details = ("Eli Lilly", "Associate Director", "Systems and Analytics")
+# QUEUES
+task_queue = ["Assignment 1", "Assignment 2", "Assignment 3"]
+customer_queue = ["Customer A", "Customer B", "Customer C"]
+booking_queue = ["10 AM Slot", "11 AM Slot", "12 PM Slot"]
+print("LIST EXAMPLES")
+print(skills)
+print(favourite_foods)
+print(learning_topics)
+print("\nTUPLE EXAMPLES")
+print(location)
+print(education)
+print(office_details)
+print("\nQUEUE EXAMPLES")
+print(task_queue)
+print(customer_queue)
+print(booking_queue)
