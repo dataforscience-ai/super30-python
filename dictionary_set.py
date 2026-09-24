c754ed400921f57fe3aa88347b49559397fd4721
dataforscience-ai <dataforscience@gmail.com>
2026-09-24 16:58:47 +0530

    Add files via upload

---
+++ dictionary_set.py
@@ -0,0 +1,55 @@
+# Dictionary 1 - Student
+
+student = {
+    "name": "Anu K V",
+    "course": "Super30",
+    "city": "Bangalore"
+}
+
+# Dictionary 2 - Employee
+
+employee = {
+    "id": 101,
+    "company": "Eli Lilly",
+    "designation": "Associate Director",
+    "location": "Bangalore"
+}
+
+# Dictionary 3 - Product
+
+product = {
+    "name": "Ayurvedic Massage",
+    "price": 2500,
+    "duration": "60 Minutes",
+    "category": "Wellness"
+}
+
+# Set 1 - Technical Skills
+
+skills = {"Analytics", "People Management", "Project Management", "AI"}
+
+# Set 2 - Technologies
+
+technologies = {"Power BI", "Power Apps", "Copilot", "Python"}
+
+# Set 3 - Cities
+
+cities = {"Bangalore", "Mumbai", "Delhi", "Bangalore"}
+
+print("Student Dictionary")
+print(student)
+
+print("\nEmployee Dictionary")
+print(employee)
+
+print("\nProduct Dictionary")
+print(product)
+
+print("\nSkills Set")
+print(skills)
+
+print("\nTechnologies Set")
+print(technologies)
+
+print("\nCities Set")
+print(cities)
---
+++ introduction.py
@@ -0,0 +1,20 @@
+name="Anu K V"
+city="Bangalore"
+profession="Associate Director-System and Analytics"
+company= "Eli Lilly"
+education="B.Tech in Biotechnology"
+goal="To become AI Director"
+hobby="Gardening,Leraning new technologies"
+dream ="Build AI product that solve real problem in Family business of Ayurvedics Spa"
+location="Bangalore"
+native_place="Kerala"
+print(name)
+print(city)
+print(profession)
+print(company)
+print(education)
+print(goal)
+print(hobby)
+print(dream)
+print(location)
+print(native_place)
