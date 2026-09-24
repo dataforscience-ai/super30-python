28408dc1ce93184e5519f58131545c76ec3eae9f
dataforscience-ai <dataforscience@gmail.com>
2026-09-24 23:53:04 +0530

    Add files via upload

---
+++ student_profile.py
@@ -0,0 +1,32 @@
+name = "Anu K V"                 # String
+age = 38                         # Integer
+height = 5.2                     # Float
+is_student = True                # Boolean
+
+city = "Bangalore"               # String
+course = "Super30"               # String
+experience = 15                  # Integer
+company = "Eli Lilly"            # String
+profession = "Associate Director" # String
+goal = "Become an AI Leader"     # String
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(height)
+print(type(height))
+print(is_student)
+print(type(is_student))
+print(city)
+print(type(city))
+print(course)
+print(type(course))
+print(experience)
+print(type(experience))
+print(company)
+print(type(company))
+print(profession)
+print(type(profession))
+print(goal)
+print(type(goal))
